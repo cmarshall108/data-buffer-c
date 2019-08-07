16b61f6d0242a45f4282ddba1cde1543667deee6
Caleb Marshall <caleb.marshall108@gmail.com>
2019-08-07 10:52:15 -0400

    buffer_iterator: make the return type for function buffer_get_remaining_data constant

--- buffer_iterator.h
+++ buffer_iterator.h
@@ -57,7 +57,7 @@ void buffer_iterator_clear(buffer_iterator_t *buffer_iterator);
 
 int buffer_read(buffer_iterator_t *buffer_iterator, size_t size, uint8_t **bytes);
 size_t buffer_get_remaining_size(buffer_iterator_t *buffer_iterator);
-uint8_t* buffer_get_remaining_data(buffer_iterator_t *buffer_iterator);
+const uint8_t* buffer_get_remaining_data(buffer_iterator_t *buffer_iterator);
 
 int buffer_read_uint8(buffer_iterator_t *buffer_iterator, uint8_t *value);
 int buffer_read_int8(buffer_iterator_t *buffer_iterator, int8_t *value);
